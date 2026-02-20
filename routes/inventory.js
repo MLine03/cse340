8102e980307b222c3bfbdbc46ec720ae259f7199
mline03 <melinelevao13@gmail.com>
2026-02-19 21:19:24 -0700

    Add inventory routes for list and detail pages

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,10 +1,11 @@
 // routes/inventory.js
+
 const express = require("express");
 const router = express.Router();
 const inventoryController = require("../controllers/inventory-controller");
 const inventoryModel = require("../models/inventory-model");
 
-// List all vehicles
+// Route: list all vehicles
 router.get("/", async (req, res, next) => {
   try {
     const vehicles = await inventoryModel.getAllVehicles();
@@ -14,7 +15,7 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-// Vehicle detail page
+// Route: vehicle detail page
 router.get("/detail/:inv_id", inventoryController.buildVehicleDetail);
 
 module.exports = router;
