1c8b68645a3daebde11024553444493c78f4be7e
mline03 <melinelevao13@gmail.com>
2026-02-20 15:25:27 -0700

    Convert accountController.js to ES modules using bcryptjs - Replaced bcrypt with bcryptjs for ES module compatibility - Updated all export statements to named exports - Ensured controller works with Node ES module deployment

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,5 +1,6 @@
-import * as accountModel from '../models/accountmodel.js';
-import bcrypt from 'bcrypt';
+// controllers/accountController.js
+import bcrypt from 'bcryptjs';
+import { getAccountById, getAccountByEmail, updateAccount, updatePassword } from '../utils/db-connection.js';
 
 export const manageAccount = async (req, res) => {
   const account = res.locals.accountData;
@@ -8,7 +9,7 @@ export const manageAccount = async (req, res) => {
 
 export const updateAccountView = async (req, res) => {
   const id = req.params.id;
-  const account = await accountModel.getAccountById(id);
+  const account = await getAccountById(id);
   res.render('account/update', { account, errors: null, message: null });
 };
 
@@ -18,33 +19,33 @@ export const updateAccountInfo = async (req, res) => {
 
   if (!firstname || !lastname || !email) errors.push('All fields are required.');
 
-  const emailExists = await accountModel.getAccountByEmail(email);
+  const emailExists = await getAccountByEmail(email);
   if (emailExists && emailExists.account_id != account_id) errors.push('Email already in use.');
 
   if (errors.length > 0) {
-    const account = await accountModel.getAccountById(account_id);
+    const account = await getAccountById(account_id);
     return res.render('account/update', { account, errors, message: null });
   }
 
-  await accountModel.updateAccount(account_id, firstname, lastname, email);
-  const account = await accountModel.getAccountById(account_id);
+  await updateAccount(account_id, firstname, lastname, email);
+  const account = await getAccountById(account_id);
   res.render('account/manage', { account, message: 'Account updated successfully!', errors: null });
 };
 
-export const updatePassword = async (req, res) => {
+export const updateAccountPassword = async (req, res) => {
   const { account_id, password } = req.body;
   const errors = [];
 
   if (!password || password.length < 8) errors.push('Password must be at least 8 characters.');
 
   if (errors.length > 0) {
-    const account = await accountModel.getAccountById(account_id);
+    const account = await getAccountById(account_id);
     return res.render('account/update', { account, errors, message: null });
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
-  await accountModel.updatePassword(account_id, hashedPassword);
+  await updatePassword(account_id, hashedPassword);
 
-  const account = await accountModel.getAccountById(account_id);
+  const account = await getAccountById(account_id);
   res.render('account/manage', { account, message: 'Password updated successfully!', errors: null });
 };
