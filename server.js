02a19776d998645d3c94dd158781594386ed50fe
mline03 <melinelevao13@gmail.com>
2026-03-26 17:59:48 -0600

    Fix module path for inventoryRoutes for deployment

--- server.js
+++ server.js
@@ -1,31 +1,35 @@
+// server.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const flash = require('connect-flash');
-const inventoryRoutes = require('./src/routes/inventoryRoutes');
-const classificationRoutes = require('./src/routes/classificationRoutes');
-const path = require('path');
+
+const inventoryRoutes = require('./routes/inventoryRoutes'); // <-- no extra src/
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'src/public')));
-app.use(bodyParser.urlencoded({ extended: true }));
+// Middleware
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(session({ secret: 'mysecret', resave: false, saveUninitialized: true }));
+app.use(session({
+  secret: 'mysecret',
+  resave: false,
+  saveUninitialized: true,
+}));
+
 app.use(flash());
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'src/views'));
-
+// Routes
 app.use('/inventory', inventoryRoutes);
-app.use('/classification', classificationRoutes);
 
-app.get('/', (req, res) => res.redirect('/inventory'));
-
-app.use((err, req, res, next) => {
-  res.status(500).render('errors/error', { error: err.message });
+// Test route
+app.get('/', (req, res) => {
+  res.send('Server is running');
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
