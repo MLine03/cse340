6b0c82710110fdcd683ffd196540787b7c85363e
mline03 <melinelevao13@gmail.com>
2026-04-03 11:20:55 -0600

    Fix controller crash by reading classification_name from query rows

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,28 +1,49 @@
 const invModel = require("../models/inventory-model")
 const utilities = require("../utilities/")
 
-// ⭐ Controller object MUST exist before using it
 const invCont = {}
 
 /* ***************************
- *  Build inventory by classification view
+ * Build inventory by classification view
  * ************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
-  try {
-    const classification_id = parseInt(req.params.classificationId)
+  const classification_id = parseInt(req.params.classificationId)
 
-    const data = await invModel.getInventoryByClassificationId(classification_id)
-    const grid = await utilities.buildClassificationGrid(data)
-    const nav = await utilities.getNav()
+  const data = await invModel.getInventoryByClassificationId(classification_id)
 
-    res.render("./inventory/classification", {
-      title: data[0].classification_name + " vehicles",
-      nav,
-      grid,
-    })
-  } catch (error) {
-    next(error)
-  }
+  // build vehicle grid
+  const grid = await utilities.buildClassificationGrid(data.rows)
+
+  // FIX: classification name comes from FIRST ROW
+  const className = data.rows[0].classification_name
+
+  const nav = await utilities.getNav()
+
+  res.render("inventory/classification", {
+    title: className + " vehicles",
+    nav,
+    grid,
+  })
+}
+
+
+/* ***************************
+ * Build vehicle detail view
+ * ************************** */
+invCont.buildByInventoryId = async function (req, res, next) {
+  const inv_id = parseInt(req.params.invId)
+
+  const data = await invModel.getInventoryByInventoryId(inv_id)
+  const vehicle = data.rows[0]
+
+  const detail = await utilities.buildVehicleDetail(vehicle)
+  const nav = await utilities.getNav()
+
+  res.render("inventory/detail", {
+    title: vehicle.inv_make + " " + vehicle.inv_model,
+    nav,
+    detail,
+  })
 }
 
 module.exports = invCont
