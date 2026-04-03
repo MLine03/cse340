2271bc312bb9c6a3d05f18db14affac92850ed75
mline03 <melinelevao13@gmail.com>
2026-04-03 00:45:43 -0600

    Fix navigation crash by returning query results from getClassifications model

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -1,43 +1,16 @@
-const pool = require("../database/connection")
+const pool = require("../database")
+
 /* ***************************
  * Get all classifications
  * ************************** */
 async function getClassifications() {
-  const sql = "SELECT * FROM classification ORDER BY classification_name"
+  const sql = `
+    SELECT * 
+    FROM classification 
+    ORDER BY classification_name
+  `
   const data = await pool.query(sql)
-  return data.rows
+  return data
 }
 
-/* ***************************
- * Get inventory by classification_id
- * ************************** */
-async function getInventoryByClassificationId(classification_id) {
-  const sql = `
-    SELECT * FROM inventory AS i
-    JOIN classification AS c
-    ON i.classification_id = c.classification_id
-    WHERE i.classification_id = $1
-  `
-  const data = await pool.query(sql, [classification_id])
-  return data.rows
-}
-
-/* ***************************
- * Get vehicle by inventory_id
- * ************************** */
-async function getInventoryById(inv_id) {
-  const sql = `
-    SELECT * FROM inventory AS i
-    JOIN classification AS c
-    ON i.classification_id = c.classification_id
-    WHERE i.inv_id = $1
-  `
-  const data = await pool.query(sql, [inv_id])
-  return data.rows[0]
-}
-
-module.exports = {
-  getClassifications,
-  getInventoryByClassificationId,
-  getInventoryById
-}
+module.exports = { getClassifications }
--- utilities/index.js
+++ utilities/index.js
@@ -1,32 +1,24 @@
 const invModel = require("../models/inventory-model")
 
-/* ******************************
- * Build Navigation
- * **************************** */
+/* ***************************
+ * Build navigation dynamically
+ * ************************** */
 async function getNav() {
   const data = await invModel.getClassifications()
-  let list = "<ul>"
-  list += '<li><a href="/">Home</a></li>'
+
+  let nav = "<ul>"
+  nav += '<li><a href="/">Home</a></li>'
+
   data.rows.forEach(row => {
-    list += `<li>
-      <a href="/inv/type/${row.classification_id}" 
-         title="See our inventory of ${row.classification_name} vehicles">
-         ${row.classification_name}
+    nav += `<li>
+      <a href="/inv/type/${row.classification_id}">
+        ${row.classification_name}
       </a>
     </li>`
   })
-  list += "</ul>"
-  return list
+
+  nav += "</ul>"
+  return nav
 }
 
-
-/* ******************************
- * Error Handler Wrapper
- * **************************** */
-function handleErrors(fn) {
-  return function (req, res, next) {
-    Promise.resolve(fn(req, res, next)).catch(next)
-  }
-}
-
-module.exports = { getNav, handleErrors }
+module.exports = { getNav }
