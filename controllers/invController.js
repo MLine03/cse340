fd795406ded0325eb38bd2bb02bbf08c5832fa8b
mline03 <melinelevao13@gmail.com>
2026-04-03 00:57:19 -0600

    Fix classification controller to render EJS view instead of sending text

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,39 +1,23 @@
 const invModel = require("../models/inventory-model")
-const utilities = require("../utilities")
+const utilities = require("../utilities/")
 
-const invCont = {}
-
-/* ***************************
+/* **************************************
  * Build inventory by classification view
- * ************************** */
+ * ************************************ */
 invCont.buildByClassificationId = async function (req, res, next) {
-  const classification_id = parseInt(req.params.classificationId)
+  const classification_id = req.params.classificationId
 
   const data = await invModel.getInventoryByClassificationId(classification_id)
-  const nav = await utilities.getNav()
 
-  res.render("inventory/classification", {
-    title: data.rows[0].classification_name + " vehicles",
+  const grid = await utilities.buildClassificationGrid(data.rows)
+
+  let nav = await utilities.getNav()
+
+  const className = data.rows[0].classification_name
+
+  res.render("./inventory/classification", {
+    title: className + " vehicles",
     nav,
-    data: data.rows
+    grid,
   })
 }
-
-
-/* ***************************
- * Build inventory detail view
- * ************************** */
-invCont.buildByInventoryId = async function (req, res, next) {
-  const inv_id = parseInt(req.params.invId)
-
-  const data = await invModel.getInventoryById(inv_id)
-  const nav = await utilities.getNav()
-
-  res.render("inventory/detail", {
-    title: data.rows[0].inv_make + " " + data.rows[0].inv_model,
-    nav,
-    vehicle: data.rows[0]
-  })
-}
-
-module.exports = invCont
