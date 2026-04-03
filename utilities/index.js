2d7a72168fb57a36db262b6de5097c2d9efa462d
mline03 <melinelevao13@gmail.com>
2026-04-02 23:13:03 -0600

    Add utilities index with handleErrors and vehicle detail builder

--- utilities/index.js
+++ utilities/index.js
@@ -1,21 +1,53 @@
-const invModel = require("../models/inventory-model")
+/* ******************************************
+* Utilities Index
+*******************************************/
 
-async function getNav() {
-  const data = await invModel.getClassifications()
+// Wrap async route controllers to catch errors
+const handleErrors = (fn) => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next)
 
-  let nav = "<ul>"
-  nav += '<li><a href="/">Home</a></li>'
-
-  data.rows.forEach(row => {
-    nav += `<li>
-      <a href="/inv/type/${row.classification_id}">
-        ${row.classification_name}
-      </a>
-    </li>`
-  })
-
-  nav += "</ul>"
-  return nav
+/* ******************************************
+* Build Navigation
+*******************************************/
+const getNav = async () => {
+  return `
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/inv/type/1">Cars</a></li>
+      <li><a href="/inv/type/2">Trucks</a></li>
+      <li><a href="/inv/type/3">SUV</a></li>
+    </ul>
+  `
 }
 
-module.exports = { getNav }
+/* ******************************************
+* Build Vehicle Detail HTML (Assignment requirement)
+*******************************************/
+const buildVehicleDetail = (vehicle) => {
+  const price = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(vehicle.inv_price)
+
+  const miles = new Intl.NumberFormat("en-US").format(vehicle.inv_miles)
+
+  return `
+    <div class="vehicle-detail">
+      <img src="${vehicle.inv_image}" alt="${vehicle.inv_make} ${vehicle.inv_model}">
+      
+      <div class="vehicle-info">
+        <h2>${vehicle.inv_year} ${vehicle.inv_make} ${vehicle.inv_model}</h2>
+        <p><strong>Price:</strong> ${price}</p>
+        <p><strong>Mileage:</strong> ${miles} miles</p>
+        <p><strong>Description:</strong> ${vehicle.inv_description}</p>
+        <p><strong>Color:</strong> ${vehicle.inv_color}</p>
+      </div>
+    </div>
+  `
+}
+
+module.exports = {
+  handleErrors,
+  getNav,
+  buildVehicleDetail,
+}
