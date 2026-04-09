464c2d608fdfd36d3e74adc4494d1ba1cacd3564
Your Name <yourgithubemail@email.com>
2026-04-09 14:48:09 -0600

    Initial commit of Assignment 4

--- server.js
+++ server.js
@@ -12,10 +12,55 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
-const utilities = require('./utilities/index')
-
+const accountRoute = require("./routes/accountRoute")
+const utilities = require("./utilities/")
+const session = require("express-session")
 const pool = require('./database/')
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
 
+/* ***********************
+ * Middleware
+ * Between the request and response
+ * ************************/
+// Unit 4, Sessions & Messages Activity
+app.use(
+  session({
+    store: new (require("connect-pg-simple")(session))({
+      createTableIfMissing: true,
+      pool,
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    name: "sessionId",
+  })
+)
+// Unit 4, Sessions & Messages Activity
+// Express Messages Middleware
+app.use(require("connect-flash")())
+app.use(function (req, res, next) {
+  res.locals.messages = require("express-messages")(req, res)
+  next()
+})
+// Unit 4, Process Registration Activity
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+// Unit 4, Process Registration Activity
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+// Unit 5 Authentication cookie use
+app.use(cookieParser())
 
 /* ***********************
  * View Engine And Templates
@@ -25,21 +70,19 @@ app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
 
-/* ***********************
- * Middleware
- * ************************/
- 
+
 
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
-//Index Route
+// Index route - Unit 3, Robust Error Handling activity
 app.get("/", utilities.handleErrors(baseController.buildHome))
-
-// Inventory routes
+// Inventory routes - Unit 3, Build Inventory route activity
 app.use("/inv", inventoryRoute)
+// Account routes - Unit 4, Deliver Login activity
+app.use("/account", accountRoute)
 
 
 
